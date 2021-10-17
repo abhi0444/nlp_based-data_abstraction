dcdd0b49a9d4c027703d95acd2fd0697524e9aec
abhimat gupta <43957648+abhi0444@users.noreply.github.com>
2021-10-17 19:48:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,35 @@ def process():
 		elif choice == 'money':
 			results = MONEY_named_entity
 			num_of_results = len(results)
-		
 	
 	return render_template("index.html",results=results,num_of_results = num_of_results)
+
+def readingTime(mytext):
+	total_words = len([ token.text for token in nlp(mytext)])
+	estimatedTime = total_words/200.0
+	return estimatedTime
+
+# Fetch Text From Url
+def get_text(url):
+	page = urlopen(url)
+	soup = BeautifulSoup(page)
+	fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+	return fetched_text
+
+@app.route('/analyze',methods=['GET','POST'])
+def analyze():
+	start = time.time()
+	if request.method == 'POST':
+		rawtext = request.form['rawtext']
+		final_summary = text_summarizer(rawtext)
+		final_summary_gensim = summarize(rawtext)
+		# NLTK
+		final_summary_nltk = nltk_summarizer(rawtext)
+		# Sumy
+		final_summary_sumy = sumy_summary(rawtext)
+		end = time.time()
+		final_time = end-start
+	return render_template('index.html',ctext=rawtext,final_summary=final_summary,final_summary_spacy=final_summary,final_summary_gensim=final_summary_gensim,final_summary_nltk=final_summary_nltk,final_summary_sumy=final_summary_sumy)
 
 
 if __name__ == '__main__':
